9aef4a69775aa7cfb7af8cf4fff943c8fe57d61f
TTXuanDao <xdao04091999@gmail.com>
2020-11-06 21:29:26 +0700

    hihihiii

--- demo.js
+++ demo.js
@@ -2,5 +2,5 @@ console.log("Khởi tạo dự án");
 // step 1 : git init
 // step 2 : add
 // step 3 : login set name and email
-
-console.log("ahihhihihihihi - sua roaiiiiiii")
+console.log(2)
+console.log("ahihhihihihihi - sua roaiiiiiii");
